d32495cf7d75b42031b99643b85c09b6c1664c29
dianamilton <dmilton3@gatech.edu>
2017-04-23 21:01:13 -0400

    Animations Update Twitter

--- app/src/main/java/com/example/dianamilton/h2go/LoginActivity.java
+++ app/src/main/java/com/example/dianamilton/h2go/LoginActivity.java
@@ -4,18 +4,25 @@ package com.example.dianamilton.h2go;
  * Created by CSquad on 2/19/17.
  */
 
+import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import android.content.Intent;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +31,10 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -31,11 +42,27 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.*;
+import com.twitter.sdk.android.core.identity.*;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
+
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 /**
  * login page for user, sends to registration page if nonregistered user, checks user and passcode
  */
 public class LoginActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "6YobnhT0ERBBTGUhQoT1tF8D8";
+    private static final String TWITTER_SECRET = "LlaMBbiYKubyWscfOWVNFLl4Txmuk404bJfjphcRItkjUJ1GYE";
+
     private static final String TAG = "LoginActivity"; //defines activity
     private static final int REQUEST_SIGNUP = 0; //defines signup request to switch screens
 
@@ -46,16 +73,30 @@ public class LoginActivity extends AppCompatActivity {
     TextView _signupLink;
     LoginButton _fbButton;
     CallbackManager callbackManager;
+    Button btnZoomOut;
+    Animation animation;
+    ImageView h2g0logo;
+    Button btnZoomIn;
 
+    TwitterLoginButton tloginButton;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    /**
      * creating link, buttons
+     *
      * @param savedInstanceState
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_login);
         callbackManager = CallbackManager.Factory.create();
 
@@ -64,6 +105,10 @@ public class LoginActivity extends AppCompatActivity {
         _loginButton = (Button) findViewById(R.id.btn_login);
         _signupLink = (TextView) findViewById(R.id.link_signup);
         _fbButton = (LoginButton) findViewById(R.id.fb_button);
+        btnZoomOut = (Button) findViewById(R.id.btnZoomOut);
+        h2g0logo =(ImageView)findViewById(R.id.h2g0);
+        btnZoomIn = (Button) findViewById(R.id.btnZoomIn);
+
 
         //sets up database
         mAuth = FirebaseAuth.getInstance();
@@ -84,6 +129,18 @@ public class LoginActivity extends AppCompatActivity {
 
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
+        tloginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        tloginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // Do something with result, which provides a TwitterSession for making API calls
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                // Do something on failure
+            }
+        });
 
 
         /**
@@ -113,7 +170,29 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        _fbButton.setOnClickListener( new View.OnClickListener() {
+        animation = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.zoom_in);
+        // Zoom In
+        btnZoomOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                h2g0logo.setVisibility(View.VISIBLE);
+                h2g0logo.startAnimation(animation);
+            }
+        });
+
+        animation = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.zoom_out);
+        // Zoom Out
+        btnZoomOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                h2g0logo.setVisibility(View.VISIBLE);
+                h2g0logo.startAnimation(animation);
+            }
+        });
+
+        _fbButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -138,6 +217,9 @@ public class LoginActivity extends AppCompatActivity {
                 });
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     /**
@@ -145,8 +227,13 @@ public class LoginActivity extends AppCompatActivity {
      */
     @Override
     public void onStart() {
-        super.onStart();
+        super.onStart();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
         mAuth.addAuthStateListener(mAuthListener);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
     }
 
     /**
@@ -154,14 +241,20 @@ public class LoginActivity extends AppCompatActivity {
      */
     @Override
     public void onStop() {
-        super.onStop();
+        super.onStop();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.disconnect();
     }
 
     /**
      * creates account using parameters under database, toast if not successful
+     *
      * @param email
      * @param password
      */
@@ -198,6 +291,7 @@ public class LoginActivity extends AppCompatActivity {
 
     /**
      * signs person in with new account using database
+     *
      * @param email
      * @param password
      */
@@ -263,7 +357,7 @@ public class LoginActivity extends AppCompatActivity {
         String email = _emailText.getText().toString();
         String password = _passwordText.getText().toString();
 
-        new android.os.Handler().postDelayed(
+        new Handler().postDelayed(
                 new Runnable() {
                     public void run() {
                         // On complete call either onLoginSuccess or onLoginFailed
@@ -287,6 +381,13 @@ public class LoginActivity extends AppCompatActivity {
 
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        //super.onActivityResult(requestCode, resultCode, data);
+        //super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result to the login button.
+        tloginButton.onActivityResult(requestCode, resultCode, data);
+
     }
 
     //MainActivity
@@ -313,6 +414,7 @@ public class LoginActivity extends AppCompatActivity {
 
     /**
      * checks if signup registration user, passcode is acceptable under requirements
+     *
      * @return
      */
     public boolean validate() {
@@ -321,7 +423,7 @@ public class LoginActivity extends AppCompatActivity {
         String email = _emailText.getText().toString();
         String password = _passwordText.getText().toString();
 
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             _emailText.setError("enter a valid email address");
             valid = false;
         } else {
@@ -338,4 +440,19 @@ public class LoginActivity extends AppCompatActivity {
         return valid;
     }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Login Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
 }
